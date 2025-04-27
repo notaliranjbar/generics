8f4247237e51afa5a420f54b1de19184c353bc63
Ali <ali123ranjbar456@gmail.com>
2025-04-27 03:57:56 -0400

    creating Product class and fields and adding setters and getters

---
+++ src/Product.java
@@ -0,0 +1,23 @@
+public class Product {
+    private String title;
+    private double price;
+    private String Id;
+    //setters and getters
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public String getId() {
+        return Id;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+}
