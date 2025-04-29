c6dd341e65f926db3a1bef7db0bfea717a6edfa7
Ali <ali123ranjbar456@gmail.com>
2025-04-29 22:28:42 +0330

    creatin calculate total price in main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.util.ArrayList;
+
 public class Main{
     public static void Main(String[] args){
 
     }
     public static double calculateTotalPrice (Inventory<? extends Product> inventory){
-        public static double sum = 0;
+        double sum = 0;
+        ArrayList<Double> prices = new ArrayList<>();
+        prices = inventory.getProdctsPrices();
+        for (Double price : prices){
+            sum += price;
+        }
+        return sum;
 
 
     }
