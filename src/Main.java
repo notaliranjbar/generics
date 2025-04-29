c058e3c228085254f507f4e15fe4244891e627e1
Ali <ali123ranjbar456@gmail.com>
2025-04-29 22:34:05 +0330

    creating some objects in main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@ import java.util.ArrayList;
 
 public class Main{
     public static void Main(String[] args){
+        Inventory inventory1 = new Inventory();
+        Inventory inventory2 = new Inventory();
+        Inventory inventory3 = new Inventory();
+        Book book1 = new Book("shazdeKocholo" , 10 , "mamad" , "publicated" , "drama");
+        NoteBook noteBook1 = new NoteBook("djls" , 12 , true , 100);
+        NoteBook noteBook2 = new NoteBook("djls" , 15 , false , 200);
+        Accessory bracelet = new Accessory("gucci bracelet" , 20934 , "gold");
+
+
 
     }
     public static double calculateTotalPrice (Inventory<? extends Product> inventory){
